bd5fcdabfe4f5cb3ed5621bba08d78898c770f19
kamal <dhimank079@gmail.com>
2022-03-07 21:05:43 +0530

    07032022

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css'
-//import '../styles/all.min.css'
-//import '../styles/flaticon.css'
-import '../styles/animate.min.css'
-import '../styles/jquery.fancybox.min.css'
-import '../styles/perfect-scrollbar.css'
-import '../styles/slick.css'
-import '../styles/globals.css'
-import '../styles/responsive.css'
-import '../styles/color3.css'
 
 
 function MyApp({ Component, pageProps }) {
