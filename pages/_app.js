d3692d0bf09e929d79dceda3d717d8b845c42146
kamal <dhimank079@gmail.com>
2022-03-20 10:30:38 +0530

    stylesheet chnagess

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,10 @@
-import Head from 'next/head'     
+
 import Script from 'next/Script'                         
 
 function MyApp({ Component, pageProps }) {
   return(
     <>
-      <Head>
-          <link rel="stylesheet" href="assets/css/all.min.css" />
-          <link rel="stylesheet" href="assets/css/flaticon.css" />
-          <link rel="stylesheet" href="assets/css/animate.min.css" />
-          <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css" />
-          <link rel="stylesheet" href="assets/css/perfect-scrollbar.css" />
-          <link rel="stylesheet" href="assets/css/slick.css" />
-          <link rel="stylesheet" href="assets/css/style.css" />
-          <link rel="stylesheet" href="assets/css/responsive.css" />
-          <link rel="stylesheet" href="assets/css/color3.css" />
-
-      </Head>
+     
 
 
     <Component {...pageProps} />
---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+          <link rel="stylesheet" href="assets/css/all.min.css" />
+          <link rel="stylesheet" href="assets/css/flaticon.css" />
+          <link rel="stylesheet" href="assets/css/animate.min.css" />
+          <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css" />
+          <link rel="stylesheet" href="assets/css/perfect-scrollbar.css" />
+          <link rel="stylesheet" href="assets/css/slick.css" />
+          <link rel="stylesheet" href="assets/css/style.css" />
+          <link rel="stylesheet" href="assets/css/responsive.css" />
+          <link rel="stylesheet" href="assets/css/color3.css" />
+
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+
+        
+      </body>
+    </Html>
+  )
+}
